506ff40347f420a665d5c693bb97238112f5ca7b
Dana Wardhiana <danawardhiana@Danas-MacBook-Pro.local>
2023-12-16 15:14:38 +0700

    First Commit

---
+++ src/Kalkulator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Kalkulator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Kalkulator Sederhana ===");
+
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = scanner.nextDouble();
+
+        System.out.print("Masukkan operasi (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = scanner.nextDouble();
+
+        double hasil = 0;
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                } else {
+                    System.out.println("Pembagian dengan nol tidak valid.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operasi tidak valid.");
+                return;
+        }
+
+        System.out.println("Hasil: " + hasil);
+    }
+}
